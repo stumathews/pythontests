ee3daaaa6921197544ece672f7f8b80c52ba1404
Stuart Robert Charles Mathews <smathews@localhost.localdomain>
2017-09-22 15:16:08 +0100

    use python3

--- Endofday.py
+++ Endofday.py
@@ -1,12 +1,11 @@
-import urllib, json
+import urllib.parse, urllib.request, json
 
 Companies = ["ARROW GLOBAL GROUP PLC","ARTEMIS ALPHA TRUST PLC"]
-Companies = [urllib.quote(company) for company in Companies]
+Companies = [urllib.parse.quote(company) for company in Companies]
 
 for company in Companies:
-    with urllib.request.urlopen("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22{company}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callack=") as url:
+    with urllib.request.urlopen(f"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22{company}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callack=") as url:
         data = json.loads(url.read().decode())
         print(data)
 
     
-
