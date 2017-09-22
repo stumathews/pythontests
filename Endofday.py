a045dc6a51d3469ac724f7b3c00934d9dd100d44
Stuart Mathews <Stuart.Mathews@citrix.com>
2017-09-22 14:37:54 +0100

    initial import of endofday for python

---
+++ Endofday.py
@@ -0,0 +1,12 @@
+import urllib, json
+
+Companies = ["ARROW GLOBAL GROUP PLC","ARTEMIS ALPHA TRUST PLC"]
+Companies = [urllib.quote(company) for company in Companies]
+
+for company in Companies:
+    with urllib.request.urlopen("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22{company}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callack=") as url:
+        data = json.loads(url.read().decode())
+        print(data)
+
+    
+
